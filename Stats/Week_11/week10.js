9625dfdd9a7e2b845bd59e59930f72e465e62d5a
areou <vandermolen.r@gmail.com>
2020-11-05 22:58:39 -0500

    week 11

---
+++ Stats/Week_11/week10.js
@@ -0,0 +1,19 @@
+var Lister=[
+"Table%20of%20Contents.html",
+"Excel Cheat Sheet.html",
+"P-Values.html",
+"P-Value Example.html",
+"Two Means Testing.html",
+"Independent VS Dependent Samples.html",
+"What do we learn from testing two means.html",
+"Types of Tests.html",
+"z-test two means.html",
+"z-test two mean EXAMPLE.html",
+"Now You Try! (z-test 2 means).html",
+"t-test two means.html",
+"t-test two means EXAMPLE.html",
+"Now You Try! (t-test 2 means).html",
+"Week 11 Vocabulary.html",
+"Week 11 Formula Sheet.html",
+"hypothesis_testing.xlsx"
+]
